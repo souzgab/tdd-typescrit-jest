a40f024a287c1c6990d2029be03298af35d1a10c
unknown <gs_arruda@outlook.com>
2020-12-03 10:39:10 -0300

    ensure LocalSavePurchases does not delete cache upon creation

---
+++ src/domain/data/usecases/local-save.purchase.spec.ts
@@ -0,0 +1,21 @@
+class LocalSavePurchases{
+    constructor(private readonly cacheStore: CacheStore){
+
+    }
+}
+
+interface CacheStore {
+
+}
+
+class CacheStoreSpy implements CacheStore {
+    deleteCallsCount = 0;
+}
+
+describe('LocalSavePurchases', () => {
+    test('Should not delete cache on sut.init', () => {
+        const cacheStore = new CacheStoreSpy()
+        const sut = new LocalSavePurchases(cacheStore)
+        expect(cacheStore.deleteCallsCount).toBe(0)
+    });
+});
