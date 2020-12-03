518631462605732aa01678992a2d61e4beee01dd
unknown <gs_arruda@outlook.com>
2020-12-03 10:49:15 -0300

    add sut maker

--- src/domain/data/usecases/local-save.purchase.spec.ts
+++ src/domain/data/usecases/local-save.purchase.spec.ts
@@ -2,20 +2,44 @@ class LocalSavePurchases{
     constructor(private readonly cacheStore: CacheStore){
 
     }
+
+    async save() : Promise<void> {
+        this.cacheStore.delete()
+    }
 }
 
 interface CacheStore {
-
+    delete: () => void
 }
 
 class CacheStoreSpy implements CacheStore {
     deleteCallsCount = 0;
+
+    delete(): void{
+        this.deleteCallsCount++;
+    }
+}
+
+type SutTypes = {
+    sut: LocalSavePurchases
+    cacheStore: CacheStoreSpy
+}
+
+const makeSut = (): SutTypes => {
+    const cacheStore = new CacheStoreSpy()
+    const sut = new LocalSavePurchases(cacheStore)
+    return {sut, cacheStore}
 }
 
 describe('LocalSavePurchases', () => {
     test('Should not delete cache on sut.init', () => {
-        const cacheStore = new CacheStoreSpy()
-        const sut = new LocalSavePurchases(cacheStore)
+        const {cacheStore} = makeSut()
         expect(cacheStore.deleteCallsCount).toBe(0)
     });
+
+    test('Should not delete cache on sut.save', async () => {
+        const {cacheStore, sut} = makeSut()
+        await sut.save()
+        expect(cacheStore.deleteCallsCount).toBe(1)
+    })
 });
